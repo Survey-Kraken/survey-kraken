4976973a4e8d54edceb2100888c4d33fda2cb0a5
Brian James <brianpja@gmail.com>
2018-01-17 14:02:07 -0800

    router added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import LandingPage from './components/LandingPage'
+import LandingPage from './components/LandingPage';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <LandingPage />
+        <Route exact path='/' render={(props) => <LandingPage />}/>
       </div>
+      </Router>
     );
   }
 }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,17 +5,21 @@ import AddQuestion from '../components/AddQuestion'
 import ViewAll from '../components/ViewAll'
 
 const LandingPage = () => {
+  function handleClick() {
+    console.log('clicking')
+  }
+
   return (
     <div>
     <h1 className="App-title">Welcome to Survey Kraken</h1>
     <p>Please begin by either creating a survey or viewing/taking a survey</p>
     <div>
-      <button>
+      <button onClick={handleClick}>
       <CreateSurvey />
       </button>
     </div>
     <div>
-      <button>
+      <button onClick={handleClick}>
       <ViewAll />
       </button>
     </div>
