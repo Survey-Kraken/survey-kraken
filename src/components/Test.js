7f1bed709e30d9d4a9224d4bb774ce2ca344b999
donkeypagoda <christopherallenstewart@gmail.com>
2018-01-17 10:24:14 -0800

    making some basic stuff to test

---
+++ src/components/Test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import CreateSurvey from '../components/CreateSurvey'
+import AddQuestion from './components/AddQuestion'
+
+
+
+
+
+export default CartItem
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,1 +1,6 @@
 import React from 'react';
+
+import AddQuestion from '../components/AddQuestion'
+import CreateSurvey from '../components/CreateSurvey'
+
+import Test from '../components/Test'
