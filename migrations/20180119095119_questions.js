af124bebceb950e0e52d777b5b209192ea338520
donkeypagoda <christopherallenstewart@gmail.com>
2018-01-19 10:53:44 -0800

    migrations are happening

--- migrations/20180119095119_questions.js
+++ migrations/20180119095119_questions.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('questions', table => {
+    table.increments();
+    table.integer('survey_id').references('surveys.id').notNullable().onDelete('CASCADE');
+    table.string('prompt').notNullable();
+    table.string('type').notNullable();
+    table.string('order').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('questions');
+
 };
