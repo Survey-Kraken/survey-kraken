32e9338a081f272770a54ca24601f17ec91fcb14
John York-Erwin <solidseussian@gmail.com>
2018-01-24 13:54:07 -0800

    thingy

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,14 +4,26 @@ import React from 'react';
 
 //code goes here
 class Question extends React.Component {
-  
+  componentWillMount() {
+    console.log(this.props)
+  }
 
   render () {
     return(
 
       <div>
-        {this.props.question.type}
-        Question!
+        <div>{this.props.question.type}</div>
+        <div>
+          <label>
+            Prompt:
+          </label>
+          <input name='prompt' type='text'/>
+          {this.props.question.choices && this.props.question.choices.length === 4 && this.props.question.choices.map(function(el, index) {
+            return <div key={index}>
+              <input type='text'/>
+            </div>
+          })}
+        </div>
       </div>
     )
   }
